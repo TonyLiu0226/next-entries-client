c9deab48ba65421b31cde376038a40f0cef4cc48
Tony Liu <tl0226yn@gmail.com>
2023-10-07 18:41:00 -0700

    implements max character limit

--- app/post/page.tsx
+++ app/post/page.tsx
@@ -16,6 +16,7 @@ export default function Home() {
     //state vars for updating content and date
     let [date, setDate] = useState('')
     let [content, setContent] = useState('')
+    let [charsRemaining, setCharsRemaining] = useState(4000)
 
     const post = async() => {
       if (date && content) {
@@ -47,6 +48,7 @@ export default function Home() {
 
     const changeContent = (e: any) => {
       setContent(e.target.value)
+      setCharsRemaining(4000 - e.target.value.length)
     }
 
     return (
@@ -62,7 +64,8 @@ export default function Home() {
               </div>
             <input type="date" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Select date" min="2000-01-01" max="2099-12-31" onChange={changeDate}/>
           </div>
-            <textarea id="message" rows={4} className="mt-10 block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Write your thoughts here..." onChange={changeContent}></textarea>
+            <textarea id="message" rows={4} className="mt-10 block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Write your thoughts here..." onChange={changeContent} maxLength={4000}></textarea>
+            <h3 className="text-red-700">{charsRemaining == 0 ? "Please limit posts to 4000 characters or less" : ""}</h3>
             <button type="button" className="py-2.5 px-5 mr-2 mb-2 mt-5 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700" onClick={post}>Alternative</button>
         </div>
       </div>
