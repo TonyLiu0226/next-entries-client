83ae19cc381fe2fa20a9f380f870e615a2edfb05
Tony Liu <tl0226yn@gmail.com>
2024-04-23 22:59:23 -0700

    centers dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -47,8 +47,10 @@ export default function DashBoard(
     }
 
     return (
-      <div className="mt-40">
-        <h1 className="font-mono font-bold text-5xl mt-10 mb-5">My Posts</h1>
+      <div className="mt-40 flex flex-col items-center">
+        <div>
+          <h1 className="font-mono font-bold text-5xl mt-10 mb-5 text-center">My Posts</h1>
+        </div>
         <div className="overflow-y-scroll h-screen" onScroll={handleScroll}>
             {postData ? postData.map((e: PostProps) => (
                 <PostCard key={uuidv4()} content={e.content} date={e.date} mood={e.mood} id={e.id}></PostCard>
